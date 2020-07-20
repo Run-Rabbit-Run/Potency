c0474543f4f2dc07c8deaa5b51f8d1be186c1596
rabbit <barateligeorge@yandex.ru>
2020-07-20 20:43:12 +0300

    1day
    head-done, slider-done, properties-in progress

---
+++ source/js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function () {
+  $(document).ready(function(){
+    $('.slider__wrapper').slick({
+      adaptiveHeight: true,
+      // autoplay: true,
+      // autoplaySpeed: 8000
+    });
+  });
+})();
